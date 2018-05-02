2a7eac47e053467d8f17847258056beccdbf08a9
yangbei <yangbei@rb2.nl>
2018-05-02 12:21:06 +0800

    add busy

---
+++ src/app/uicontrols/busy-indicator/busy-indicator.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+
+export const gbTopBusyMsg = '正在为您加载应用.';
+
+@Injectable()
+export class BusyIndicatorService {
+  isBusy = false;
+  busyMsg: string = gbTopBusyMsg;
+
+  constructor() { }
+
+  startWork(msg: string = null) {
+    if (msg)
+      this.busyMsg = msg;
+    this.isBusy = true;
+  }
+
+  doneWork() {
+    this.isBusy = false;
+    this.busyMsg = gbTopBusyMsg;
+  }
+}
+
+
+@Component({
+  selector: 'app-busy-indicator',
+  templateUrl: './busy-indicator.component.html',
+  styleUrls: ['./busy-indicator.component.css']
+})
+export class BusyIndicatorComponent implements OnInit {
+
+  constructor(private bs: BusyIndicatorService) { }
+
+  ngOnInit() {
+  }
+}
--- src/app/uicontrols/uicontrols.module.ts
+++ src/app/uicontrols/uicontrols.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { BusyIndicatorComponent } from './busy-indicator/busy-indicator.component';
+import { BusyIndicatorService } from './busy-indicator/busy-indicator.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SearchBoxComponent],
-  exports: [SearchBoxComponent]
+  declarations: [SearchBoxComponent, BusyIndicatorComponent],
+  exports: [SearchBoxComponent, BusyIndicatorComponent],
+  providers: [BusyIndicatorService]
 })
 export class UicontrolsModule { }
 
